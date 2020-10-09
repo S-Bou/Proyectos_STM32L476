1b21fef694769a1917e89d0444149e9425348c22
S-Bou <55810268+S-Bou@users.noreply.github.com>
2020-10-09 10:30:55 +0200

    0910201030

--- BlinkLed/MDK-ARM/BlinkLed/startup_stm32l476xx.d
+++
@@ -1,1 +0,0 @@
-blinkled\startup_stm32l476xx.o: startup_stm32l476xx.s
--- Practica_810/MDK-ARM/STM32L476RG_NUCLEO/output/cars.d
+++
@@ -1,3 +0,0 @@
-.\stm32l476rg_nucleo\output\cars.o: ..\Src\cars.c
-.\stm32l476rg_nucleo\output\cars.o: ..\Src\cars.h
-.\stm32l476rg_nucleo\output\cars.o: D:\Programas\STMicroelectronics\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
