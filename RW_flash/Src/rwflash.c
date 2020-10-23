0a9d00717129d0324d594ce4ae32adb97cf87d02
S-Bou <55810268+S-Bou@users.noreply.github.com>
2020-10-23 22:26:44 +0200

    2310202226

--- RW_flash/Src/rwflash.c
+++ RW_flash/Src/rwflash.c
@@ -10,71 +10,78 @@
 #include "rwflash.h"
 #include "string.h"
 /********************************************************************************************************************************************************/
-/*Declaration of struct to store the information give by user.*/
+/*Declaration of struct to store the information give by user */
 FlashDirAndPage FlashDirStruct;
 /********************************************************************************************************************************************************/
 /**
   * @brief  This function configure a led to indicate if ocurs any fail and set the direction of where user whis start to write memory 
   * @param  Address specifies the address to be programmed.
   * @param  Page specifies the page to be programmed.
-  * @retval None
+  * @retval None.
   */
 void InitFlashRW(uint32_t bank, uint32_t address, uint32_t page)
 {
-	/*Call to configuration of pin to use user led*/
+	/*Call to configuration of pin to use user led */
 	GPIO_Init_UserLED();
-	/*Set the address given by user in the struct*/
+	/*Set the address given by user in the struct */
 	FlashDirStruct.DDU   = address;
-	/*Set the page given by user in the struct*/
+	/*Set the page given by user in the struct */
 	FlashDirStruct.PAGE = page;
 }
 /********************************************************************************************************************************************************/
 /**
-  * @brief  This function...
-  * @param  None
-  * @retval None
+  * @brief  This function erase the page defined by the user before write the data.
+  * @param  UserData contain the data defined by the user.
+  * @retval ErrorCode can be used to know the tipe of erros ocurrs.
+  * @retval All_OK can be used to know error has occurred and what kind is.
   */
 uint32_t WritePageInFlash(uint64_t userData[])
 {
+	/*Declaration of variables */
 	uint32_t ErrorCode = 0;
 	uint32_t addres = GetMemDir();
-	uint32_t page =  GetPageDir();
+	uint32_t pageSelected =  GetPageDir();
+	/*Find out size of vector of data.*/
 	FlashDirStruct.LOND = sizeof(userData);
-	
+	/*Declaration of struct to configuration the mode of erase */
 	FLASH_EraseInitTypeDef EraseInitStruct;
-
+	/*Configuration the mode of erase.*/
 	EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
-	EraseInitStruct.Page = page ;
-
+	//EraseInitStruct.Banks = FLASH_BANK_1;           
+	//EraseInitStruct.NbPages = 1;
+	EraseInitStruct.Page = pageSelected;
+	/*Unlock the FLASH control register access */
 	HAL_FLASH_Unlock();
 
-	  /* Clear OPTVERR bit set on virgin samples */
+	/* Clear OPTVERR bit set on virgin samples */
   __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_OPTVERR); 
-	
+	/*Erase the specified FLASH memory pages */
 	if(HAL_FLASHEx_Erase(&EraseInitStruct, &ErrorCode) != HAL_OK)
 	{
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);	// indicator for erasing fails
-		return ErrorCode = HAL_FLASH_GetError();
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);	/*indicator for erasing fails */
+		return ErrorCode = HAL_FLASH_GetError();	/*Return value of code if error */
 	}
 	
 	for(uint32_t i = 0; i < FlashDirStruct.LOND; i++)
 	{
+		/*Program double word of a row at a specified address */
 		if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, addres, userData[i]) == HAL_OK)
 		{
-			addres += 8;
+			addres += 8;	/*Increase the direction to write next word */
 		}else{
-			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);	// indicator for program fails
-			return ErrorCode = HAL_FLASH_GetError();
+			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);	/*indicator for program fails */
+			return ErrorCode = HAL_FLASH_GetError();	/*Return value of code if error */
 		}	
 	}
+	/*Lock the FLASH control register access */
 	HAL_FLASH_Lock();
-	
+	/*Return a value if all finished fine */
 	return ALL_OK;
 }
 /********************************************************************************************************************************************************/
 /**
-  * @brief  This function...
-  * @param  None
+  * @brief  This function allows show in console the value in the specificated memory by user.
+  * @param  Address specifies the address to be programmed.
   * @retval None
   */
 void ReadMemDir(uint32_t addres)
@@ -88,32 +95,35 @@ void ReadMemDir(uint32_t addres)
 }
 /********************************************************************************************************************************************************/
 /**
-  * @brief  This function...
+  * @brief  This function get the direction of memory defined by the user.
   * @param  None
-  * @retval None
+  * @retval FlashDirStruct.DDU contain the direction defined by the user.
   */
 uint32_t  GetMemDir(void)
 {
+	/*Return the direction of memory defined by the user */
 	return FlashDirStruct.DDU;
 }
 /********************************************************************************************************************************************************/
 /**
-  * @brief  This function...
+  * @brief  This function get the number of page defined by the user.
   * @param  None
-  * @retval None
+  * @retval FlashDirStruct.PAGE contain the number of the page defined by the user.
   */
 uint32_t  GetPageDir(void)
 {
+	/*Return the number of the page defined by the user */
 	return FlashDirStruct.PAGE;
 }
 /********************************************************************************************************************************************************/
 /**
-  * @brief GPIO Initialization Function
+  * @brief GPIO initialization function to configure PA5 as output.
   * @param None
   * @retval None
   */
 void GPIO_Init_UserLED(void)
 {
+	/*Declaration of struct to configuration the mode of  pin */
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
--- RW_flash/Src/rwflash.h
+++ RW_flash/Src/rwflash.h
@@ -16,25 +16,25 @@ extern "C" {
 /********************************************************************************************************************************************************/
 #include "stm32l4xx_hal.h"
 #include "stdio.h"
-#define ALL_OK 666
+#define ALL_OK 0x666
 /********************************************************************************************************************************************************/
 /**
   * @brief  Flash direction and data structure definition
   */
 typedef struct
 {
-	uint32_t DDU;    /*This parameter contain the direction defined by the user.*/
+	uint32_t DDU;    /*This parameter contain the direction defined by the user */
 	
-	uint32_t PAGE;	/*This parameter contain the number of page where start the direction of memory.*/
+	uint32_t PAGE;	/*This parameter contain the number of page where start the direction of memory */
 	
-	uint32_t LOND;
+	uint32_t LOND;	/*This parameter contain the longitud of  vector data defined by the user */
 	
-	uint32_t BANK_X;
+	uint32_t BANK_X;  /*This parameter contain the bank of memory defined by the user */
 	
 } FlashDirAndPage;
 
-/* Peripheral Control functions  **********************************************/
-/** @addtogroup FLASH_Exported_Functions_Group2
+/* Peripheral Control functions  **********************************************************************************************************************/
+/** @brief  FLASH options functions definition
   * @{
   */
 void InitFlashRW(uint32_t bank, uint32_t address, uint32_t page);
